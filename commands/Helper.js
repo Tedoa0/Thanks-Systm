5481c9bc36d056816b32cee222d3a2f6c7a30796
Tedoa0 <77301986+Tedoa0@users.noreply.github.com>
2021-06-07 12:32:11 +0300

    Update Helper.js

--- commands/Helper.js
+++ commands/Helper.js
@@ -1,9 +1,3 @@
-const { MessageEmbed } = require("discord.js");
-const db = require("quick.db");
-const config = require('../config.json')
-const ayarlar = require('../ayarlar.json')
-const emoji = require('../emoji')
-
 exports.run = async(client, message, args) => {
 
 message.channel.send(`<@&${config.helpers.helper}> (${message.author})`)

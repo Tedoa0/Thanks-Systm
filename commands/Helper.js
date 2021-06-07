9e62e3acb5ca53f1614103d56691d3d889c8868e
Tedoa0 <77301986+Tedoa0@users.noreply.github.com>
2021-06-07 21:01:02 +0300

    Update Helper.js

--- commands/Helper.js
+++ commands/Helper.js
@@ -1,6 +1,7 @@
 const emoji = require('../emoji')
+const config = require('../config.json')
 exports.run = async(client, message, args) => {
-
+ if(message.channel.id !== "Kanal İd") return message.channel.send('Bu komutu sadece <#Kanal ID> Kanalında Kullanabilirsin').then(tedoa => tedoa.delete({ timeout : 10000 })) 
 message.channel.send(`<@&${config.helpers.helper}> (${message.author})`).then(message.react(emoji.onayemoji))
   
     
